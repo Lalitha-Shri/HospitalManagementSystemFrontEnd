a4532d4d3d748fd6c206025ab8a1720e5b5dc9ae
Lalitha-Shri <153735153+Lalitha-Shri@users.noreply.github.com>
2024-04-05 12:20:21 +0530

    Update AdminComponent.jsx

--- hospitalManagementFrontEnd/src/components/AdminComponent.jsx
+++ hospitalManagementFrontEnd/src/components/AdminComponent.jsx
@@ -6,7 +6,10 @@ import { logout } from './AuthService';
 import Swal from 'sweetalert2'
 
 const AdminComponent = () => {
+  //React hook
   const navigate=useNavigate();
+  //handlelogout is called when logout button is clicked in cards
+  //it clears session storage and local storage by making call to logout function in Authservice
   const handleLogOut=()=>{
     logout();
     navigate("/");
@@ -41,7 +44,7 @@ const AdminComponent = () => {
             <Card.Title>Doctor's List</Card.Title>
             <Card.Text>View the list of doctors.</Card.Text>
             <Link to="/listDoctor">
-              <Button variant="danger">Go to doctor's List</Button>
+              <Button variant="danger">//Go to doctor's List</Button>//Go to doctor list by rendering DoctorComponent
             </Link>
           </Card.Body>
         </Card>
@@ -52,7 +55,7 @@ const AdminComponent = () => {
             <Card.Title>Patient's List</Card.Title>
             <Card.Text>View the list of patient.</Card.Text>
             <Link to="/listPatient">
-              <Button variant="danger">Go to Patient's List</Button>
+              <Button variant="danger">Go to Patient's List</Button>//Go to doctor's List</Button>//Go to patient list by rendering PatientComponent
             </Link>
           </Card.Body>
         </Card>
@@ -63,7 +66,7 @@ const AdminComponent = () => {
             <Card.Title>Add Doctors</Card.Title>
             <Card.Text>Add a new doctor to the system.</Card.Text>
             <Link to="/addDoctor">
-              <Button variant="danger">Add Doctors</Button>
+              <Button variant="danger">Add Doctors</Button>//Go to add doctor form  by rendering AddDoctor
             </Link>
           </Card.Body>
         </Card>
